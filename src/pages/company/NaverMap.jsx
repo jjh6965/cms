be5ba8d5533e805aaff506f9d256c8e7ae87272b
jjh6965 <jaehun6965@gmail.com>
2025-08-02 11:01:49 +0900

    250802_navermap

--- src/pages/company/NaverMap.jsx
+++ src/pages/company/NaverMap.jsx
@@ -25,6 +25,7 @@ const MapComponent = () => {
         console.log("Fetching from:", `${apiUrl}/api/naver/client-id`);
         const response = await axios.get(`${apiUrl}/api/naver/client-id`, {
           withCredentials: true,
+          timeout: 15000, // 15초 타임아웃
         });
         console.log("Response data:", response.data);
         setClientId(response.data.clientId);
@@ -35,8 +36,10 @@ const MapComponent = () => {
           error.response ? error.response.data : {}
         );
         if (error.response && error.response.status === 401) {
-          //console.warn("401 발생, 서버 인증 문제 확인 필요");
-          setClientId("o2rbo106py"); // 새로 발급받은 client-id 사용
+          setClientId("o2rbo106py"); // 임시 방편
+        } else if (error.code === "ECONNABORTED") {
+          console.error("Request timed out, retrying...");
+          setTimeout(fetchClientId, 2000); // 2초 후 재시도
         }
       } finally {
         setLoading(false);
